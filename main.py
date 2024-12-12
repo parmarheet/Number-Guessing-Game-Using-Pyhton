fa1f16e0be636cce2baea7f30479a9074cd6c2e1
Parmar Heet <parmarheet12@gmail.com>
2024-12-12 11:52:54 +0530

    Number Guessing Game Using Pyhton

---
+++ main.py
@@ -0,0 +1,29 @@
+import random
+
+def guess_the_number():
+    print("Welcome to 'Guess the Number' game!")
+    print("I'm thinking of a number between 1 and 100.")
+    print("Try to guess it in as few attempts as possible.")
+
+    # Generate a random number between 1 and 100
+    number_to_guess = random.randint(1, 100)
+    attempts = 0
+
+    while True:
+        try:
+            # Get the user's guess
+            user_guess = int(input("Enter your guess: "))
+            attempts += 1
+
+            if user_guess < number_to_guess:
+                print("Too low! Try again.")
+            elif user_guess > number_to_guess:
+                print("Too high! Try again.")
+            else:
+                print(f"Congratulations! You guessed the number in {attempts} attempts.")
+                break
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
+if __name__ == "__main__":
+    guess_the_number()
